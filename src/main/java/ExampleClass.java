aa73f4088d246f452364ff5ffef054fad91ee57c
Larissa Marcich <lmarcich@thoughtworks.com>
2017-06-22 13:33:47 -0500

    Example field annotation

---
+++ src/main/java/ExampleClass.java
@@ -0,0 +1,8 @@
+public class ExampleClass {
+    @FieldAnnotation("example annotation message")
+    Object someField;
+
+    public ExampleClass(Object someField){
+        this.someField = someField;
+    }
+}
